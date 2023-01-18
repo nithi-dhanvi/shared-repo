45486eeb01aa0ef94e5e33a4e2d5d68adfefc77b
nithi-dhanvi <ramesheee64@gmail.com>
2023-01-18 12:57:39 -0600

    test com

--- vars/stage2test.groovy
+++ vars/stage2test.groovy
@@ -3,7 +3,7 @@ def call(String imageName) {
 
 
     sh "docker build -t $imageName ."
-    sh "docker tag $imageName $imageName':$BUILD_NUMBER'"
+    sh "docker tag $imageName $username/$imageName':$BUILD_NUMBER'"
     sh "echo $password | docker login -u $username --password-stdin "
     sh "docker push $imageName':$BUILD_NUMBER' "
     sh "docker rmi $imageName':$BUILD_NUMBER' "
