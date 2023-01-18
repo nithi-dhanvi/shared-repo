e74d2bc945efe8862032f21da6d5f3aacc2987b8
nithi-dhanvi <ramesheee64@gmail.com>
2023-01-18 12:31:04 -0600

    test com

--- vars/stage2test.groovy
+++ vars/stage2test.groovy
@@ -1,9 +1,11 @@
 def call(String imageName) {
-    withCredentials ([usernamePassword(credentialsId: "dockerhub-cred", usernameVariable: "Username", passwordVariable: "Password")])
+    withCredentials ([usernamePassword(credentialsId: "dockerhub-cred", usernameVariable: "username", passwordVariable: "password")]) {
+
+
     sh "docker build -t $imageName ."
     sh "docker tag $imageName $imageName':$BUILD_NUMBER'"
     sh "docker push $imageName':$BUILD_NUMBER' "
     sh "docker rmi $imageName':$BUILD_NUMBER' "
-
+    }
 }
 
