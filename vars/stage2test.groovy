f2c7aceb8dff7b2c991d84a6c52552d7a0afb4e6
nithi-dhanvi <ramesheee64@gmail.com>
2023-01-18 12:18:27 -0600

    test com

--- vars/stage2test.groovy
+++ vars/stage2test.groovy
@@ -1,9 +1,29 @@
-def call() {
-    sh "/opt/apache-maven-3.6.3/bin/mvn clean package -DskipTests=true"
-    echo " inside call func"
-    withSonarQubeEnv("sonar") {
-        sh "/opt/apache-maven-3.6.3/bin/mvn sonar:sonar"
-        }
+def call(String imageName) {
+    withCredtials ([usernamePassword(CredentialsId: "dockerhub-cred", usernameVariable: "username", passwordVariable: "password"])
+    
 
 }
-       
+
+registry = "pothakamuri06/webapp" 
+        registryCredential = 'dockerhub-cred' 
+        dockerImage = ''
+
+stage ("Build the docker image") {
+            steps {
+                script {
+                dockerImage = docker.build registry + ":$BUILD_NUMBER" 
+                sh "docker images"
+                }
+            }
+        }
+              
+        stage ("Push image to dockerhub") {
+            steps {
+                script {
+                     docker.withRegistry( '', registryCredential ) { 
+                        dockerImage.push()
+                                                         
+                    }
+                }
+            }
+        }
